9b346afb300c12633bf195acd7b12deab5a6b537
Zandisiwe Ndhlovu <ndhlovuzandisiwe@gmail.com>
2023-04-19 13:57:25 +0200

    Created a Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './style.css'
+import Header from './components/Header'
 
 function App() {
-  return <div>App</div>
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
 }
 
 export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import troll from '../images/troll_face.svg'
 
 function Header() {
-  return <div>Header</div>
+  return (
+    <header className="header background">
+      <div className="logo">
+        <img src={troll} alt="troll face" className="troll-img" />
+        <h2 className="header--title">Meme Generator</h2>
+      </div>
+      <p className="header--project">React Course - Project 3</p>
+    </header>
+  )
 }
 
 export default Header
